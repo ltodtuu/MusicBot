4f5419e720a6416810eb9a2b5ee8c56252f64339
Laurin Sebastian Todt <laurin.todt@uni-ulm.de>
2024-06-18 21:37:21 +0200

    feat: add initial song queue

---
+++ Backend/api/songs/songs.go
@@ -0,0 +1,64 @@
+package songs
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+type Song struct {
+	Title      string  `json:"title"`
+	Artist     string  `json:"artist"`
+	Album      string  `json:"album"`
+	SpotifyURI url.URL `json:"spotify_uri"`
+	TidalURI   url.URL `json:"tidal_uri"`
+}
+
+type SongQueue struct {
+	songs []Song
+	mutex sync.Mutex
+}
+
+var queue = &SongQueue{}
+
+func (q *SongQueue) GetNextSong() (Song, bool) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	if len(q.songs) == 0 {
+		return Song{}, false
+	}
+	nextSong := q.songs[0]
+	q.songs = q.songs[1:]
+	return nextSong, true
+}
+
+func (q *SongQueue) AddSong(song Song) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	q.songs = append(q.songs, song)
+}
+
+func AddSongHandler(w http.ResponseWriter, r *http.Request) {
+	var song Song
+	if err := json.NewDecoder(r.Body).Decode(&song); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	queue.AddSong(song)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func NextSongHandler(w http.ResponseWriter, r *http.Request) {
+	song, ok := queue.GetNextSong()
+	if !ok {
+		http.Error(w, "No songs in queue", http.StatusNotFound)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(song); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"Backend/api/songs"
+	"log"
+	"net/http"
+)
 
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("GET /songs/next", songs.NextSongHandler)
+	router.HandleFunc("POST /songs/add", songs.AddSongHandler)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Println("Server listening on port :8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 }
