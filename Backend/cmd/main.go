5e8a5a159420608a122c1c8c4104ae41e4d9656d
Laurin Todt <l.todt@outlook.de>
2024-06-23 15:46:55 +0200

    feat: add config

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"Backend/api/songs"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Port          int         `json:"port"`
+	TidalSettings TidalConfig `json:"tidal_settings"`
+}
+
+type TidalConfig struct {
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
 func main() {
+	config, errCfg := LoadConfig("appsettings.json")
+	if errCfg != nil {
+		log.Fatalf("Failed to load configuration: %s", errCfg)
+	}
+
 	router := http.NewServeMux()
 	router.HandleFunc("GET /songs/next", songs.NextSongHandler)
 	router.HandleFunc("GET /songs/queue", songs.ReadQueueHandler)
 	router.HandleFunc("POST /songs/add", songs.AddSongHandler)
 
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    fmt.Sprintf(":%d", config.Port),
 		Handler: router,
 	}
 
